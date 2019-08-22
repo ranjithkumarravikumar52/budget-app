8c6ac578a40be72d63688c29b283bf90cd1e20c2
ranjithkumarravikumar52 <ranjithkumarravikumar52@gmail.com>
2019-08-22 14:38:00 +0530

    created bare-bone UIContrller and appController as modules. Closes #1.

--- app.js
+++ app.js
@@ -12,7 +12,29 @@ var budgetController = (function () {
     //return an object that we want it to be public
     return {
         publicTest : function(b){
-            console.log(addFunction(b));
+            return addFunction(b);
         }
     }
 })();
+
+var UIController = (function(){
+    //some code
+})();
+
+//the main controller that connects all the other controllers
+var appController = (function(budgetCtrl, UICtrl){ //params are named differently to avoid name-space collision and confusion with global variables
+
+    //we want to access any function from the param but not from global
+    // budgetController.publicTest(10); //bad practice
+
+    //sample test of budgetController
+    var z = budgetCtrl.publicTest(10);
+
+    //return in an object
+    return {
+        anotherPublicTest : function(){
+            console.log("z", z);
+        }
+    }
+
+})(budgetController, UIController);
